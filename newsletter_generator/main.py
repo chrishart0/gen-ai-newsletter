cd67086eec0fbea651053e509afdda515770c908
Phil Simpson <phillip.simpson@cognizant.com>
2024-12-04 16:23:40 -0500

    modify prompt to cite sources

--- newsletter_generator/main.py
+++ newsletter_generator/main.py
@@ -15,11 +15,12 @@ logger = get_logger()
 # List of URLs to process
 # TODO: make some method of finding the right newsletter issues every week, for now we update by hand
 gen_ai_newsletter_urls = [
-    "https://www.deeplearning.ai/the-batch/issue-275/",
+    "https://www.deeplearning.ai/the-batch/issue-277/",
     "https://www.deeplearning.ai/the-batch/issue-276/",
     # "https://www.reddit.com/r/LocalLLaMA/"
     # Add more URLs as needed
 ]
+
 
 def generate_news_data():
     # Initialize a list to store all events
@@ -28,13 +29,17 @@ def generate_news_data():
     for url in gen_ai_newsletter_urls:
         loaded_webpage = load_webpage(url)
         events_list = extract_news_items(loaded_webpage, url)
-        all_news_items.extend(events_list.news_items)  # Add individual events to the list
+        all_news_items.extend(
+            events_list.news_items
+        )  # Add individual events to the list
 
     # Get the pydantic objects into a list of dicts we can store in a JSON file
     serializable_events = [event.dict() for event in all_news_items]
 
     # Store events in a JSON file
-    logger.info(f"Storing {len(all_news_items)} events in {settings.OUTPUT_DIRECTORY}events.json")
+    logger.info(
+        f"Storing {len(all_news_items)} events in {settings.OUTPUT_DIRECTORY}events.json"
+    )
     with open(f"{settings.OUTPUT_DIRECTORY}events.json", "w") as f:
         json.dump(serializable_events, f, indent=4, default=str)
 
@@ -70,7 +75,8 @@ Keep the newsletter exciting and engaging, but don't make up any news items.
 Group the news items into sections with a short introductory paragraph for each section.
 When discussing models, cite evals/benchmarks/etc., compare to other popular models as much as possible so people can easily understand the relative performance of the models.
 Create a short executive summary at the top, maximum 4 sentences. In the executive summary, create an open loop, exciting readers, and giving a short hint at the most important topics. Someone should be able to read the executive summary get the most important headlines.
-"""
+Be sure to cite sources for all news items by linking to the original source.
+""",
         ),
         ("human", json.dumps(events_data)),
     ]
@@ -81,7 +87,6 @@ Create a short executive summary at the top, maximum 4 sentences. In the executi
     # Write the newsletter content to a markdown file
     with open(f"{settings.OUTPUT_DIRECTORY}newsletter.md", "w") as f:
         f.write(newsletter_author_response.content)
-
 
 
 if __name__ == "__main__":
